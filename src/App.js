0fd027ac6193c459d46da48e586c64c19bbed6d3
Gaurav Edekar <edekar.gaurav@gmail.com>
2021-09-19 18:56:12 +0200

    WIP Storybook, example with theme using ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import MUIButton from '@material-ui/core/Button';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <ThemeProvider theme={darkTheme}>
+        <header className="App-header">
+          <GlobalStyles />
+          <MUIButton
+              color="primary"
+              variant="contained"
+          >TEST</MUIButton>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        </ThemeProvider>
+      </div>
   );
 }
 
