7e5bee6aa8b88f67e67c6440fe5b40e1dabf8626
Gaurav Edekar <edekar.gaurav@gmail.com>
2021-10-27 20:35:38 +0200

    WIP, asynchronous autocomplete

--- src/App.js
+++ src/App.js
@@ -1,47 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
 import MUIButton from '@material-ui/core/Button';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from './theme';
-import { GlobalStyles } from './global';
+import namesData from './assets/data/names.json'
+import {ThemeProvider} from 'styled-components';
+import {lightTheme, darkTheme} from './theme';
 
-import { createTheme } from '@material-ui/core/styles';
+import {GlobalStyles} from './global';
+import {createTheme} from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
+import {TextField} from "@material-ui/core";
+import {Autocomplete} from "@mui/material";
 
-const theme = createTheme({
+const pageConfigState = {
+  "showAutocompletePage": false,
+  "showHomePage": true
+}
+const names = Array.from(new Set(namesData.names));
+
+const theme = createTheme(createTheme({
   palette: {
-    primary: purple,
-    secondary: green,
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#ff4400',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#0066ff',
+      main: '#0044ff',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
   },
-  status: {
-    danger: 'orange',
-  },
-});
+}));
 
 function App() {
   return (
       <div className="App">
         <ThemeProvider theme={theme}>
-        <header className="App-header">
-          <GlobalStyles />
-          <MUIButton
-              color="primary"
-              variant="contained"
-          >TEST</MUIButton>
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <header className="App-header">
+            <GlobalStyles/>
+            <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={names}
+                sx={{width: 300}}
+                renderInput={(params) => <TextField {...params} label="Names"/>}
+            />
+            <MUIButton
+                color="primary"
+                variant="contained"
+            >TEST</MUIButton>
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
         </ThemeProvider>
       </div>
   );
